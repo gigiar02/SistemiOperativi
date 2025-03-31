e5f2951378b003a71587d934a592d35d106309f2
gigi <luigiarianopio02@gmail.com>
2025-03-31 20:37:20 +0200

    Fine esercizio processi

--- Processi/es_1/main.c
+++ Processi/es_1/main.c
@@ -1,4 +1,8 @@
-
+/*
+	Creato il 31/03/2025 alle ore 20:35
+	
+	La traccia è nella cartella stessa
+*/
 
 
 
@@ -11,13 +15,9 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#define N 18
 
-#define N 100
-
-
-pid_t  pidList[N];
-int glob = 0;
-
+//Svolgimento dell'esercizio
 void esercizio(int arg)
 {
 	int pid;
@@ -27,7 +27,7 @@ void esercizio(int arg)
 	
 	
 	
-	//Scelta azione da fare
+	//Conteggio pari e dispari
 	for(int i = 0; i < arg; i++)
 	{
 		int random = rand() % (arg + 1);
@@ -54,34 +54,31 @@ void esercizio(int arg)
 		
 		}else
 		{
-			glob++;
-			printf("glob: %d \n",glob);
+			//Figlio
 			id = i;
-			pidList[id] = getpid();
-			printf("sono id = %d pid = %d \n",id,pidList[id]);
 			
-			if(id -1 < 0) return;
-			if(id - N/2 < 0) return;
+			printf("sono id = %d pid = %d \n",id,getpid());
 			
-			if(pari > disp)
-			{
-				if(id % 2 == 0)
-				{
+			//Casi Base
+			if(id -1 < 0 && pari > disp) return;
+			if(id - N/2 < 0 && pari <= disp) return;
+			
+			
+			if(pari > disp && id % 2 == 0)
+			{	
+				printf("[+] Id = %d invio sengale: SIGUSR1 a %d \n",id,getpid()-1);
+				kill(getpid()-1,SIGUSR1);
 					
-					printf("[+] Id = %d invio sengale: SIGUSR1 a %d \n",id,getpid()-1);
-					kill(pidList[id-1],SIGUSR1);
-					
-				}
-			}else
+				
+			}else if(pari <= disp && id > N/2)
 			{
 				//ogni processo i > N/2 Manda un segnale SIGUSR2 al processo con identificatore i-N/2
-				if(id > N/2)
-				{
-					printf("[+] Id = %d invio sengale: SIGUSR2 a %d \n",id,getpid() -N/2);
-					kill(pidList[id-N/2],SIGUSR2);
-				}
+				printf("[+] Id = %d invio sengale: SIGUSR2 a %d \n",id,getpid() -N/2);
+				kill(getpid()-N/2,SIGUSR2);
+				
 			}
 			
+			sleep(100);
 			return;
 		}
 	
@@ -103,14 +100,16 @@ void esercizio(int arg)
 
 }
 
+
+//Gestore del segnale
 void handle(int type)
 {
 	if(type == SIGUSR1)
 	{
-		printf("Ricevuto SIGUSR1 \n");
+		printf("Sono %d e ho ricevuto SIGUSR1 \n",getpid());
 	}else if(type == SIGUSR2)
 	{
-		printf("Ricevuto SIGUSR2 \n");
+		printf("Sono %d e ho ricevuto SIGUSR2 \n",getpid());
 	}
 }
 
@@ -120,6 +119,8 @@ int main(int argc, char *argv[])
 	
 	signal(SIGUSR1,handle);
 	signal(SIGUSR2,handle);
+	
+	//Controllo argomenti
 	if(argc < 2)
 	{
 		perror("Too few arguments: ");
@@ -129,5 +130,5 @@ int main(int argc, char *argv[])
 	int number = atoi(argv[1]);
 	esercizio(number);
 	
-	
+	return 0;
 }
